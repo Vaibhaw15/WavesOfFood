b6a3e165c6e977454fc909b85bf91aca9cff570d
Vaibhaw Soni <vaibhawsoni900@gmail.com>
2024-11-07 21:28:59 +0530

    loginScreen Ui done

--- app/src/main/java/com/example/wavesoffood/StartActivity.kt
+++ app/src/main/java/com/example/wavesoffood/StartActivity.kt
@@ -1,20 +1,26 @@
 package com.example.wavesoffood
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.wavesoffood.databinding.ActivityStartBinding
 
 class StartActivity : AppCompatActivity() {
+    private val binding: ActivityStartBinding by lazy {
+        ActivityStartBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.nextButton.setOnClickListener {
+            println("Button clicked")
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
         }
     }
 }
