549c81757a2876c839320d7aab8e6bd5e4b8cd5e
Vaibhaw Soni <vaibhawsoni900@gmail.com>
2024-11-07 17:16:26 +0530

    Spalsh Screen Done

---
+++ app/src/main/java/com/example/wavesoffood/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.wavesoffood
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intend = Intent(this,MainActivity::class.java)
+            startActivity(intend)
+            finish()
+        },3000)
+    }
+}
