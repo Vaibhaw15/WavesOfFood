8e0f7d54dd35518321915b67f889ea97f5a2285c
Vaibhaw Soni <vaibhawsoni900@gmail.com>
2024-11-08 14:27:07 +0530

    Choose location Ui Done

---
+++ app/src/main/java/com/example/wavesoffood/ChooseLocationActivity.kt
@@ -0,0 +1,26 @@
+package com.example.wavesoffood
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.wavesoffood.databinding.ActivityChooseLocationBinding
+
+class ChooseLocationActivity : AppCompatActivity() {
+    private val binding : ActivityChooseLocationBinding by lazy {
+        ActivityChooseLocationBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        val locationList : Array<String> = arrayOf("jaipur"," Odisha","Jharkhand")
+        val adapter : ArrayAdapter<String> = ArrayAdapter(this,android.R.layout.simple_list_item_1,locationList)
+        val autoCompleteTextView : AutoCompleteTextView = binding.listOfLocation
+        autoCompleteTextView.setAdapter(adapter)
+
+        }
+}
