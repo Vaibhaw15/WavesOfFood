bb98aee329271215a7635cdb6cef195c8de98a7b
Vaibhaw Soni <vaibhawsoni900@gmail.com>
2024-11-23 08:43:47 +0530

    congrats bottomSheet done

--- app/src/main/java/com/example/wavesoffood/Fragment/CartFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragment/CartFragment.kt
@@ -1,11 +1,13 @@
 package com.example.wavesoffood.Fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wavesoffood.PayOutActivity
 import com.example.wavesoffood.R
 import com.example.wavesoffood.adapter.CartAdapter
 import com.example.wavesoffood.databinding.FragmentCartBinding
@@ -32,6 +34,11 @@ class CartFragment : Fragment() {
         val adapter = CartAdapter(ArrayList(cartFoodName),ArrayList( cartItemPrice), ArrayList(cartImage))
         binding.cartRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.cartRecyclerView.adapter = adapter
+
+        binding.procedButton.setOnClickListener({
+            val intent  = Intent(requireContext(), PayOutActivity::class.java)
+            startActivity(intent)
+        })
         return binding.root
 
     }
---
+++ app/src/main/java/com/example/wavesoffood/PayOutActivity.kt
@@ -0,0 +1,25 @@
+package com.example.wavesoffood
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.wavesoffood.databinding.ActivityPayOutBinding
+
+class PayOutActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPayOutBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityPayOutBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        binding.placeMyOrder.setOnClickListener{
+            val bottomSheet = CongratsBottomSheet()
+            bottomSheet.show(supportFragmentManager, "Test")
+        }
+        binding.buttonBackOrder.setOnClickListener{
+            finish()
+        }
+    }
+}
